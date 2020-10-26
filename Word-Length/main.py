80b399c122a414a4fa71a6e4260f99b49e7b6fe3
Rami RB <ramib202@gmail.com>
2020-10-25 20:34:56 -0400

    My first version

---
+++ Word-Length/main.py
@@ -0,0 +1,57 @@
+
+
+
+def word_count(my_string):
+    previous_space = True
+    words = 1
+    try:
+        for i in my_string:
+            if i == " ":
+                if previous_space == False:
+                    previous_space = True
+                    words += 1
+            elif not (i == " "):
+                previous_space = False
+        return words
+    except:
+        return "Not a string"
+
+def word_length(my_string):
+    previous_space = False
+    char = 0
+    if len(my_string) > 0:
+        for i in my_string:
+            if i == " ":
+                if previous_space == False:
+                    previous_space = True
+                    char += 0
+            elif i == ".":
+                char += 0
+            elif i == ",":
+                char += 0
+            elif i == "!":
+                char += 0
+            elif i == "?":
+                char += 0
+            elif not (i == " "):
+                previous_space = False
+                char += 1
+        if char == 0:
+            return "No words"
+    return char
+
+
+
+def average_word_length(my_string):
+  if word_count(my_string) == "Not a string":
+    return "Not a string"
+  elif word_length(my_string) == "No words":
+    return "No words"
+  else:
+    avg = word_length(my_string) / word_count(my_string)
+    return avg
+
+
+
+print("hi")
+print(average_word_length("Hello Hi hey"))
